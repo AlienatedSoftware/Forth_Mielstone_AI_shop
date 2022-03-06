84a904281340fe326522382515b05b4db45cf5b8
AlienatedSoftware <aidancgart@gmail.com>
2022-03-06 23:49:56 +0000

    Added class methods to webhook handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -11,3 +11,15 @@ class StripeWH_Handler:
         return HttpResponse(
             content=f'Unhandled webhook received: {event["type"]}',
             status=200)
+
+    # Handles the payment_intent.succeeded webhook from Stripe
+    def handle_payment_intent_succeeded(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    # Handles the payment_intent.payment_failed webhook from Stripe
+    def handle_payment_intent_payment_failed(self, event):
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
