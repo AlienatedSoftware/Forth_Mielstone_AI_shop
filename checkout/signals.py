ff34da3cc6324ce52c2c9579ffd3c21ba415eda3
AlienatedSoftware <aidancgart@gmail.com>
2022-03-06 15:24:35 +0000

    Implemented forms and signals as well as updating admin panel

---
+++ checkout/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import OrderLineItem
+
+# Updates order total on the lineitem create/update function
+@receiver(post_save, sender=OrderLineItem)
+def update_on_save(sender, instance, created, **kwargs):
+    instance.order.update_total()
+
+# Updates order total on the lineitem delete function
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
+    instance.order.update_total()
