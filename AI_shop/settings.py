d8890cc2e93b5a1351c21c821445f266ae5120e8
AlienatedSoftware <aidancgart@gmail.com>
2022-03-07 07:57:42 +0000

    Deployment part 2

--- AI_shop/settings.py
+++ AI_shop/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-1j!6%o!*npk0ai$5)4+1s5q_d-=whg%okt=m5_)0uh8gm4rh4p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['artificial-intelligence-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -121,12 +121,17 @@ WSGI_APPLICATION = 'AI_shop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
- }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
